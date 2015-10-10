7f9e867fce4521622db2231d1e4ec2f3deddf0e2
alanerzhao <alanerzhao@gmail.com>
2015-10-10 15:07:48 +0800

    add comment example ok

--- src/js/comment/comment.js
+++ src/js/comment/comment.js
@@ -4,10 +4,12 @@
  * state是组件私有的
  * this.setState来改变状态，react是状态驱动的
  * 静态用props 动态用state
+ * react使用驼峰命名规范
+ * refs 属性给子组件命名，this.refs引用对象getDOMNode获取引用对象对应的dom节点
  */
 var converter = new showdown.Converter();
 //实例一个commentBox组件
-var CommentBox = React.createClass({
+var CommentBox = React.createClass({//{{{
 
     getInitialState: function () {
         return {data:[]}
@@ -20,32 +22,39 @@ var CommentBox = React.createClass({
             url: '../json/' + this.props.url,
             dataType:"json",
             success: function (data) {
-                setTimeout(function () {
-                    self.setState({
-                        data:data
-                    })
-                },1000)
+                self.setState({
+                    data:data
+                })
             },
             error: function () {
-                console.log("error")
+                console.log("error");
             }
         })
     },
+    //callback 更新状态
+    handleCommentSubmit: function (comment) {
 
+        this.state.data.push(comment);
+
+        this.setState({
+            data:this.state.data
+        })
+
+        console.log("refresh the List");
+    },
     render: function () {
 
     var commentBox = <div className="commentBox">
-            <h1>Comments</h1>
+            <h3>Comments</h3>
             <CommentList data={this.state.data}/>
-            <CommentForm />
+            <CommentForm onCommentSubmit={this.handleCommentSubmit} />
         </div>
 
         return ( commentBox );
     }
-});
+});//}}}
 
-
-var CommentList = React.createClass ({
+var CommentList = React.createClass ({//{{{
     render: function () {
         var data = this.props.data;
         var commentItem = data.map(function(item,index) {
@@ -55,24 +64,14 @@ var CommentList = React.createClass ({
         })
 
         return (
-            <div className="commentList">
+            <div className="commentList mui-panel">
                 {commentItem}
             </div>
         )
     }
-})
+})//}}}
 
-var CommentForm = React.createClass ({
-    render: function () {
-        return (
-            <div className="commentForm">
-            Lorem ipsa reprehenderit obcaecati eligendi vel. Eos sit dolorum exercitationem earum obcaecati possimus, libero illo eveniet non sed rem! Aspernatur magnam enim autem sunt debitis veritatis aliquid veniam quam distinctio.
-            </div>
-        )
-    }
-})
-
-var Comment = React.createClass ({
+var Comment = React.createClass ({//{{{
     render: function () {
         //TODO Waring: XSS html
         var rawMarkup = converter.makeHtml(this.props.children.toString())
@@ -83,7 +82,45 @@ var Comment = React.createClass ({
             </div>
         )
     }
-})
+})//}}}
+
+var CommentForm = React.createClass ({//{{{
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.refs.author.getDOMNode().value.trim();
+        var text = this.refs.text.getDOMNode().value.trim();
+        if(!text || !author) {
+            console.log("empty")
+            return;
+        }
+
+
+        //TODO 发送数据到服务器然后清空 && callback
+        this.props.onCommentSubmit({
+            author:author,
+            text:text
+        })
+
+        this.refs.author.getDOMNode().value = '';
+        this.refs.text.getDOMNode().value = '';
+
+        return;
+    },
+    render: function () {
+        return (
+            <form className="commentForm mui-panel" onSubmit={this.handleSubmit}>
+                <legend>new Comment</legend>
+                <div className="mui-textfield">
+                    <input type="text" className="mui-textfield__input" ref="author" placeholder="Input 1" />
+                </div>
+                <div className="mui-textfield">
+                    <textarea className="mui-textfield__input" ref="text" placeholder="Textarea" />
+                </div>
+                <button type="submit" className="mui-btn mui-btn--raised">Submit</button>
+            </form>
+        )
+    }
+})//}}}
 
 var fetchData = [
     {author: "Pete Hunt", text: "This is one comment"},
